519f91297ced618ba62926452f5821d3557acc3f
shinyingrid <144208471+shinyingrid@users.noreply.github.com>
2024-05-23 09:54:28 +0530

    Update GitPullPushDemo.java

--- src/ArrayPgms/GitPullPushDemo.java
+++ src/ArrayPgms/GitPullPushDemo.java
@@ -2,5 +2,11 @@ package ArrayPgms;
 
 public class GitPullPushDemo
 {
+	public static void main(String [] args)
+	{
 	//Push 1: From Eclipse
+
+	//Push 1: From Eclipse
+	System.out.println(" hi ");
+}
 }
